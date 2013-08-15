8fe5d27918716ff1ab06d9998a43eef361230e11
engel lee <engel0088@gmail.com>
2013-08-15 18:04:34 +0800

    initial empty project commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hiex::Application.config.secret_token = '2f71961d584600894414a5f9caff49f364444c6f13001032d1c0b978ae15a543cae419cb4c59a9157ad1aceda9c0fdbade6c994b2e4e7ea36bb1285e55f998ff'
