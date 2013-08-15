e5312063aca6dc2adcd7ca466b861321b97ce1e6
engel lee <engel0088@gmail.com>
2013-08-15 21:56:19 +0800

    update migrations

--- db/migrate/20130815133326_create_anonymous_questionnaires.rb
+++ db/migrate/20130815133326_create_anonymous_questionnaires.rb
@@ -1,8 +1,14 @@
 class CreateAnonymousQuestionnaires < ActiveRecord::Migration
   def change
     create_table :anonymous_questionnaires do |t|
+      t.integer :anonymous_id
+      t.integer :question_id
+      t.integer :response_id
+      t.string :response_text
 
       t.timestamps
     end
+
+    add_index :anonymous_questionnaires, :anonymous_id
   end
 end
--- db/migrate/20130815133412_create_bonus.rb
+++ db/migrate/20130815133412_create_bonus.rb
@@ -1,6 +1,10 @@
 class CreateBonus < ActiveRecord::Migration
   def change
-    create_table :bonus do |t|
+    create_table :bonuses, :force => true do |t|
+      t.integer :sales_person_id
+      t.float :amount
+      t.string :note
+      t.integer :payment_id
 
       t.timestamps
     end
